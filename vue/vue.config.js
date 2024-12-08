4e324683b748eccc9e0af56463e7d22f1bcb15ef
sxsun1684 <s.x.sun1684@gmail.com>
2024-12-08 05:18:09 -0800

    deploy

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,6 +1,11 @@
 const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
+    // Specify the base URL for production and development
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/market-platform/' // Replace '/my-project/' with your actual production base path
+        : '/',
+
     devServer: {
         port: 8080
     },
@@ -10,5 +15,6 @@ module.exports = defineConfig({
                 args[0].title = "Bunny Market";
                 return args;
             })
-    }
+    },
+
 })
