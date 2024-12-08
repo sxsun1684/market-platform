97c8ea8c9a8b26b68981f76f23f128875d09d025
sxsun1684 <s.x.sun1684@gmail.com>
2024-12-08 06:27:20 -0800

    deploy

--- vue/deploy.sh
+++ vue/deploy.sh
@@ -3,11 +3,8 @@
 # abort on errors
 set -e
 
-# navigate to the frontend directory
-cd frontend
-
-# build the project
-npm run build
+## build
+#npm run build
 
 # navigate into the build output directory
 cd dist
@@ -20,7 +17,8 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:sxsun1684/market-platform.git main:gh-pages
+git push -f git@github.com:sxsun1684/market-platform.git master:gh-pages
 
-# return to the root directory
+
+
 cd -
--- vue/vue.config.js
+++ vue/vue.config.js
@@ -6,6 +6,8 @@ module.exports = defineConfig({
         ? '/market-platform/' // Replace '/my-project/' with your actual production base path
         : '/',
 
+    indexPath: './dist/public/index.html',
+
     devServer: {
         port: 8080
     },
